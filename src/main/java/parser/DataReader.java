fafe76323c175d2e1341ef3c69de069e691b32b0
Xu3is <mleliukov@yandex.ru>
2026-01-30 13:08:42 +0300

    made data types analyze

--- src/main/java/parser/DataReader.java
+++ src/main/java/parser/DataReader.java
@@ -4,22 +4,41 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataReader {
 
-    private int intCount;
-    private int doubleCount;
-    private int stringCount;
+    private final List<BigInteger> ints = new ArrayList<>();
+    private final List<Double> doubles = new ArrayList<>();
+    private final List<String> strings = new ArrayList<>();
+
+
+    private void EndifyType(String line) {
+        try {
+            BigInteger num = new BigInteger(line);
+            ints.add(num);
+            return;
+        } catch (NumberFormatException _) {}
+
+        try {
+            double numdub = Double.parseDouble(line);
+            doubles.add(numdub);
+            return;
+        } catch (NumberFormatException _) {}
+
+        strings.add(line);
+    }
 
     public void readFile(Path filePath) {
-
         try (BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 line = line.trim();
                 if (!line.isEmpty()) {
-                    System.out.println(line);
+                    EndifyType(line);
                 }
             }
         } catch (FileNotFoundException e) {
@@ -27,5 +46,11 @@ public class DataReader {
         } catch (IOException e) {
             System.err.println("Ошибка чтения файла " + e.getMessage());
         }
+    }
+
+    public void testprintall() {
+        System.out.println(ints);
+        System.out.println(doubles);
+        System.out.println(strings);
     }
 }
--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 public class Main {
     static void main() {
         DataReader reader = new DataReader();
-        reader.readFile(Path.of("input/in2.txt"));
+        reader.readFile(Path.of("input/in1.txt"));
+        reader.testprintall();
     }
 }
