fbdf4f28b5b7504441b2281287a90037956b1395
Xu3is <mleliukov@yandex.ru>
2026-02-06 02:00:12 +0300

    made error handlig for -o option

--- src/main/java/parser/CommandLineArgumentsParser.java
+++ src/main/java/parser/CommandLineArgumentsParser.java
@@ -13,35 +13,49 @@ public class CommandLineArgumentsParser {
             if (current.equals("-s")) {
                 cli.SetShortStat();
                 cli.SetStatistic(new Statistics());
-
             } else if (current.equals("-f")) {
                 cli.SetFullStat();
                 cli.SetStatistic(new Statistics());
+
             } else if (current.equals("-p")) {
                 if (i + 1 >= args.length) {
-                    System.out.println("Для утилинты -p нужен префикс, опция -p игнорируется");
+                    System.out.println("Для опции -p нужен префикс, опция игнорируется");
                     continue;
                 }
-                String next = args[i + 1];
-                if (next.startsWith("-")) {
+                String prefixParse = args[i + 1];
+                if (prefixParse.startsWith("-")) {
                     System.out.println("После опции -p должен идти префикс, опция игнорируется");
                     continue;
                 }
-                if (next.trim().isEmpty()) {
+                if (prefixParse.trim().isEmpty()) {
                     System.out.println("Префикс пустой, опция -p игнорируется");
                     continue;
                 }
-                if (next.toLowerCase().endsWith(".txt")) {
-                    System.out.println("Префикс не может быть файлом, опиция -p игнорируется");
+                if (prefixParse.toLowerCase().endsWith(".txt")) {
+                    System.out.println("Префикс не может быть файлом, опция -p игнорируется");
                     continue;
                 }
-                cli.setPrefix(next);
+                cli.setPrefix(prefixParse);
                 i++;
             } else if (current.startsWith("-o")) {
                 if (i + 1 >= args.length) {
-                    throw new IllegalArgumentException("Для -o нужен путь");
+                    System.out.println("Для опции -o нужен путь, опция игнориуется, файлы созданы в рабочей папке");
+                    continue;
                 }
-                cli.setOutputPath(Paths.get(args[i + 1]));
+                String pathParse = args[i + 1];
+                if (pathParse.startsWith("-")) {
+                    System.out.println("После опции -о должен идти путь, опция игнориуется, файлы созданы в рабочей папке");
+                    continue;
+                }
+                if (pathParse.trim().isEmpty()) {
+                    System.out.println("Путь пустой, -o опция игнориуется, файлы созданы в рабочей папке");
+                    continue;
+                }
+                if (pathParse.toLowerCase().endsWith(".txt")) {
+                    System.out.println("Путь не может быть файлом, опция -о игнорируется, файлы созданы в рабочей папке");
+                    continue;
+                }
+                cli.setOutputPath(Paths.get(pathParse));
                 i++;
             } else if (current.equals("-a")){
                 cli.setAppendable();
